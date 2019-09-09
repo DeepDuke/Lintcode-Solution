d6adb23140d269653a301572940a82f38e14aa40
deepduke <runningwater.jc@gmail.com>
2019-09-09 17:28:27 +0800

    two pointers

---
+++ 464.sort-integers-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param A: an integer array
+     * @return: nothing
+     */
+    void sortIntegers2(vector<int> &A) {
+        // write your code here
+       sort(A.begin(),  A.end());
+    }
+};
